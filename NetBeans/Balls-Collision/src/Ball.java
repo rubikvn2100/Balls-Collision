affef2c62e205337a440ac219faefada11299844
JavaRogers <40445970+JavaRogers@users.noreply.github.com>
2018-07-19 19:27:36 -0700

    Updated for use with Netbeans
    Add a project in NetBeans so you can make proper changes!

---
+++ NetBeans/Balls-Collision/src/Ball.java
@@ -0,0 +1,14 @@
+// Authors: 	Khoa Le, Huyen Nguyen, & Jove Rogers
+// School: 	Chabot College
+// Instructor:	Manny Kang
+// Course:	CSCI-19A
+// Create date:	19 July 2018
+// Unlicensed
+
+/**
+ *
+ * 
+ */
+public class Ball {
+
+}
