b27c87fd94d5e6a9d966a7553d51126e50f24866
JavaRogers <40445970+JavaRogers@users.noreply.github.com>
2018-07-24 21:39:11 -0700

    Started writing some methods

--- AndroidStudio/BallsCollision/app/src/main/java/nguyenlerogers/balls_collision/Field.java
+++ AndroidStudio/BallsCollision/app/src/main/java/nguyenlerogers/balls_collision/Field.java
@@ -1,73 +1,60 @@
 package nguyenlerogers.balls_collision;
 
-public class Field {
-    /** Data Fields **/
-    private Ball ballList[]; // Todo: Should represent a POINTER to a BallList
-    private BallPair pairList[][];// Todo: Should represent a POINTER to a BallPair
-    private HoldBall holdList[];
-    private boolean justCollision[][];
-    private Wall fieldWall; // Todo: Should represent a POINTER to a Wall
-    private int fieldSize;
-    private Ball ballToCollision; // Todo: Should represent a POINTER to a Ball
-    private BallPair pairToCollision; // Todo: Should represent a POINTER to a BallPair
+import java.time.temporal.TemporalAdjusters;
 
+public class Field {
+    /** Data Fields, sans getters (private) **/
+    private Ball ballList[]; // Array of pointer to list of Balls / Todo: Should represent a POINTER to a BallList
+    private BallPair pairList[][];// Array of pointer to list of pair Todo: Should represent a POINTER to a BallPair
+    private HoldBall holdList[]; // Hold the old value of ball list
+    private boolean justCollision[][]; // Check list of just collision ball
+    private Wall fieldWall; // Pointer to the wall / Todo: Should represent a POINTER to a Wall
+    private int fieldSize; // The size of our field
+    private Ball ballToCollision; // Type of next collision / Todo: Should represent a POINTER to a Ball
+    private BallPair pairToCollision; // Type of next collision / Todo: Should represent a POINTER to a BallPair
+
+    /** Data Fields, with getters (public) **/
+    private double fieldTime;
+    private double nextCollisionTime;
+    private boolean updated;
+
+    /** Constructors **/
+    Field(int fieldSize, int leftX, int topY, int rightX, int bottomY, double t) {
+        System.out.println("Field object was created");
+        this.fieldSize = fieldSize;
+        this.updated = true;
+
+
+        // Todo: Code Field(asdfjkl); constructor
+
+    }
     /** Getter Methods **/
-    public Ball[] getBallList() {
-        return ballList;
+    public double getFieldTime() {
+        return this.fieldTime;
     }
-    public BallPair[][] getPairList() {
-        return pairList;
+    public double getNextCollisionTime() {
+        return this.nextCollisionTime;
     }
-    public HoldBall[] getHoldList() {
-        return holdList;
-    }
-    public boolean[][] getJustCollision() {
-        return justCollision;
-    }
-    public Wall getFieldWall() {
-        return fieldWall;
-    }
-    public int getFieldSize() {
-        return fieldSize;
-    }
-    public Ball getBallToCollision() {
-        return ballToCollision;
-    }
-    public BallPair getPairToCollision() {
-        return pairToCollision;
+    public boolean isUpdated() {
+        return this.updated;
     }
 
     /** Setter Methods **/
-    public void setBallList(Ball[] ballList) {
-        this.ballList = ballList;
+    public void setFieldTime(double fieldTime) {
+        this.fieldTime = fieldTime; // Todo: Is this method... necessary? Is it correct?
     }
-    public void setPairList(BallPair[][] pairList) {
-        this.pairList = pairList;
+    public void setNextCollisionTime(double nextCollisionTime) {
+        this.nextCollisionTime = nextCollisionTime; // Todo: I'm just making setter methods. Remove this if necessary.
     }
-    public void setHoldList(HoldBall[] holdList) {
-        this.holdList = holdList;
-    }
-    public void setJustCollision(boolean[][] justCollision) {
-        this.justCollision = justCollision;
-    }
-    public void setFieldWall(Wall fieldWall) {
-        this.fieldWall = fieldWall;
-    }
-    public void setFieldSize(int fieldSize) {
-        this.fieldSize = fieldSize;
-    }
-    public void setBallToCollision(Ball ballToCollision) {
-        this.ballToCollision = ballToCollision;
-    }
-    public void setPairToCollision(BallPair pairToCollision) {
-        this.pairToCollision = pairToCollision;
+    public void setUpdated(boolean updated) {
+        this.updated = updated;
     }
 
-    /** Other Methods **/
-    public void upToDate(double foo) {
+    /** Private Methods **/
+    private void upToDate(double foo) {
         // Todo: Code Field.upToDate method
     }
-    public boolean isBallSeparate(Ball foo/*Todo: POINTER*/, int bar) {
+    private boolean isBallSeparate(Ball foo/*Todo: POINTER*/, int bar) {
         // Todo: Code Field.isBallSeparate method
         return false;
     }
