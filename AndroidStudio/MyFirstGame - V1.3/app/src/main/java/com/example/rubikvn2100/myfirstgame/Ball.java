08fbb9f737e7c8ec47df548236c5b75ae3a6e734
rubikvn2100 <rubikvn2100@gmail.com>
2018-07-31 20:33:03 -0700

    Update the color to V1.3

--- AndroidStudio/MyFirstGame - V1.3/app/src/main/java/com/example/rubikvn2100/myfirstgame/Ball.java
+++ AndroidStudio/MyFirstGame - V1.3/app/src/main/java/com/example/rubikvn2100/myfirstgame/Ball.java
@@ -8,6 +8,7 @@ public class Ball
 {
     private Point center;
     private Vector velocity;
+    private boolean useColorTemperature = true;
 
     private int radius;
     private double mass;
@@ -109,12 +110,63 @@ public class Ball
         center.setX( center.getX() + velocity.getX() * timePass );
         center.setY( center.getY() + velocity.getY() * timePass );
         time += timePass;
+
+        this.updateColor();
     }
 
     public void draw( Canvas canvas )
     {
         canvas.drawOval( (float)(center.getX() - radius), (float)(center.getY() + radius),
                 (float)(center.getX() + radius), (float)(center.getY() - radius), color );
+    }
+
+    public void updateColor()
+    {
+        if( !useColorTemperature )
+        {
+            this.color.setColor(Color.rgb(255, 255, 255));
+            return;
+        }
+
+        double scalar = 0.15; // TODO : Color Temperature scalar will need some adjustment
+        int temperature = (int)(scalar * this.getVelocity().getLength());
+        int red, green, blue;
+
+        /* Calculate Red */
+        if( temperature <= 66 )
+            red = 255;
+        else
+        {
+            red = temperature - 60;
+            red = (int)(329.698727446 * Math.pow(red,-0.1332047592));
+        }
+
+        red = (red > 255) ? 255 : red;
+
+        /* Calculate Green */
+        if( temperature <= 66  )
+        {
+            green = temperature;
+            green = (int)(99.4708025861 * Math.log(green) - 161.1195681661);
+        }
+        else
+            {
+            green = temperature - 60;
+            green = (int)(288.1221695283 * Math.pow(green,-0.0755148492));
+        }
+        green = (green > 255) ? 255 : green;
+
+        /* Calculate Blue */
+        if( temperature >= 66 )
+            blue = 255;
+        else
+        {
+            blue = temperature - 10;
+            blue = (int)(138.5177312231 * Math.log(blue) - 305.0447927307);
+        }
+        blue = (blue > 255) ? 255 : blue;
+
+        this.color.setColor(Color.rgb(red, green, blue));
     }
 
     public String toString()
