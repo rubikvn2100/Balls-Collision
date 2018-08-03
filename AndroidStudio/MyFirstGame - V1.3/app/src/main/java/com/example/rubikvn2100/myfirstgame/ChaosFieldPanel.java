4baaf64b480e79a9d776707d9ef3cb4f1cbbdad6
JavaRogers <40445970+JavaRogers@users.noreply.github.com>
2018-08-02 18:45:58 -0700

    Random velocities

--- AndroidStudio/MyFirstGame - V1.3/app/src/main/java/com/example/rubikvn2100/myfirstgame/ChaosFieldPanel.java
+++ AndroidStudio/MyFirstGame - V1.3/app/src/main/java/com/example/rubikvn2100/myfirstgame/ChaosFieldPanel.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import java.util.Random;
 
 class ChaosFieldPanel extends GamePanel
 {
@@ -18,20 +19,33 @@ class ChaosFieldPanel extends GamePanel
     {
         playGround = new PlayGround( 0,0, getWidth() - 0, getHeight() - 0 );
         playGround.setColor( 0, 0, 0 );
+        Random rando = new Random();
 
         for( int i = 0; i < 250; i++ )
         {
-            playGround.addBall( 5, 10, 5 );
+            double vx = rando.nextDouble()*10;
+            double vy = rando.nextDouble()*10;
+            if(rando.nextBoolean()) vx *= -1;
+            if(rando.nextBoolean()) vy *= -1;
+            playGround.addBall( vx, vy, 5 );
         }
 
         for( int i = 0; i < 125; i++ )
         {
-            playGround.addBall( 5, 10, 10 );
+            double vx = rando.nextDouble()*10;
+            double vy = rando.nextDouble()*10;
+            if(rando.nextBoolean()) vx *= -1;
+            if(rando.nextBoolean()) vy *= -1;
+            playGround.addBall( vx, vy, 10 );
         }
 
         for( int i = 0; i < 25; i++ )
         {
-            playGround.addBall( 5, 10, 40 );
+            double vx = rando.nextDouble()*10;
+            double vy = rando.nextDouble()*10;
+            if(rando.nextBoolean()) vx *= -1;
+            if(rando.nextBoolean()) vy *= -1;
+            playGround.addBall( vx, vy, 40 );
         }
         /*
         for( int i = 0; i < 5; i++ )
